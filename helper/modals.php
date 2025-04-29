ea31445a5ba63ea217bf63c5af38b363f1980d2c
RonDev <124356275+HerrStoeckchen@users.noreply.github.com>
2025-04-29 18:32:14 +0200

    Modals angepasst
    Modals wurden Angepasst und überarbeitet. Ein Danke Modal wurde Designt und soll nun noch mit Animationen versehen werden.

--- helper/modals.php
+++ helper/modals.php
@@ -77,8 +77,8 @@
         <p class="fw-light lh-1 mb-2">
           <span class="fw-semibold">Seitenbetreiber:</span><br>
           Ronny Melzer<br>
-          Am Stadion 4<br>
-          07629 Hermsdorf<br>
+          <a href="https://osm.org/go/0MA8tL~A?m=" rel="noreferrer noopener" class="custom-link" target="_blank">Am Stadion 4<br>
+          07629 Hermsdorf</a><br>
           <br>
           <span class="fw-semibold">Kontakt</span><br>
           E-Mail: <?= email('1em', 'currentColor'); ?>
@@ -179,22 +179,39 @@
       <div class="modal-body JetBrainsVersionen">
         <p>Danke geht raus an:</p>
 
-        <div class="btn-group btn-group-sm mb-1" role="group" aria-label="Small button group">
-          <button class="btn btn-outline-primary">Bootstrap Logo</button>
-          <a type="button" class="btn btn-outline-primary">Name & Link Button</a>
-          <button id="konfetti-bootstrap" class="btn btn-outline-primary">Konfetti mit Bootstrap-Logo</button>
+        <div class="d-flex border rounded overflow-hidden button-group">
+          <div class="d-flex justify-content-center align-items-center svg-container">
+            <svg height="2em" xmlns="http://www.w3.org/2000/svg" shape-rendering="geometricPrecision" text-rendering="geometricPrecision" image-rendering="optimizeQuality" fill-rule="evenodd" clip-rule="evenodd" viewBox="0 0 512 408">
+              <defs>
+                <linearGradient id="a" gradientUnits="userSpaceOnUse" x1="76" y1="10" x2="523" y2="365">
+                  <stop offset="0" stop-color="#9013FE" />
+                  <stop offset="1" stop-color="#6610F2" />
+                </linearGradient>
+              </defs>
+              <path fill="url(#a)" fill-rule="nonzero" d="M56.48 53.32C55.52 25.58 77.13 0 106.34 0H405.7c29.21 0 50.82 25.58 49.86 53.32-.93 26.65.27 61.16 8.96 89.31 8.72 28.23 23.41 46.08 47.48 48.37v26c-24.07 2.29-38.76 20.14-47.48 48.37-8.69 28.15-9.89 62.66-8.96 89.31.96 27.74-20.65 53.32-49.86 53.32H106.34c-29.21 0-50.82-25.58-49.86-53.32.93-26.65-.28-61.16-8.96-89.31C38.8 237.14 24.07 219.29 0 217v-26c24.07-2.29 38.8-20.14 47.52-48.37 8.68-28.15 9.89-62.66 8.96-89.31z" />
+              <path fill="#fff" d="M342.9 251.1c0 38.2-28.5 61.36-75.8 61.36h-89.2v-217h88.74c39.44 0 65.32 21.35 65.32 54.13 0 23.01-17.4 43.62-39.59 47.22v1.21c30.2 3.31 50.53 24.21 50.53 53.08zm-130.49 33.84v-71.43h45.6c32.66 0 49.61 12.03 49.61 35.49s-16.48 35.94-47.6 35.94h-47.61zm0-161.96h45.91c24.96 0 39.13 11.13 39.13 31.28 0 21.5-16.48 33.53-46.37 33.53h-38.67v-64.81z" />
+            </svg>
+          </div>
+          <a href="https://getbootstrap.com/" rel="noreferrer noopener" target="_blank" class="d-flex justify-content-center align-items-center link-button">
+            Bootstrap
+          </a>
+          <div id="konfetti-bootstrap" class="d-flex justify-content-center align-items-center action-button">
+            Konfetti
+          </div>
         </div>
-        <div class="btn-group btn-group-sm" role="group" aria-label="Small button group">
-          <button type="button" class="btn btn-outline-primary">Github</button>
-          <button type="button" class="btn btn-outline-primary">Name & Link Button</button>
-          <button type="button" id="konfetti-github" class="btn btn-outline-primary">Konfetti mit Github-Logo</button>
+        <div class="d-flex border rounded overflow-hidden button-group mt-1">
+          <div class="d-flex justify-content-center align-items-center svg-container">
+            <svg height="2em" xmlns="http://www.w3.org/2000/svg" width="64" height="64" shape-rendering="geometricPrecision" text-rendering="geometricPrecision" image-rendering="optimizeQuality" fill-rule="evenodd" clip-rule="evenodd" viewBox="0 0 640 640">
+              <path d="M319.988 7.973C143.293 7.973 0 151.242 0 327.96c0 141.392 91.678 261.298 218.826 303.63 16.004 2.964 21.886-6.957 21.886-15.414 0-7.63-.319-32.835-.449-59.552-89.032 19.359-107.8-37.772-107.8-37.772-14.552-36.993-35.529-46.831-35.529-46.831-29.032-19.879 2.209-19.442 2.209-19.442 32.126 2.245 49.04 32.954 49.04 32.954 28.56 48.922 74.883 34.76 93.131 26.598 2.882-20.681 11.15-34.807 20.315-42.803-71.08-8.067-145.797-35.516-145.797-158.14 0-34.926 12.52-63.485 32.965-85.88-3.33-8.078-14.291-40.606 3.083-84.674 0 0 26.87-8.61 88.029 32.8 25.512-7.075 52.878-10.642 80.056-10.76 27.2.118 54.614 3.673 80.162 10.76 61.076-41.386 87.922-32.8 87.922-32.8 17.398 44.08 6.485 76.631 3.154 84.675 20.516 22.394 32.93 50.953 32.93 85.879 0 122.907-74.883 149.93-146.117 157.856 11.481 9.921 21.733 29.398 21.733 59.233 0 42.792-.366 77.28-.366 87.804 0 8.516 5.764 18.473 21.992 15.354 127.076-42.354 218.637-162.274 218.637-303.582 0-176.695-143.269-319.988-320-319.988l-.023.107z" />
+            </svg>
+          </div>
+          <a href="https://github.com/" rel="noreferrer noopener" target="_blank" class="d-flex justify-content-center align-items-center link-button">
+            Github
+          </a>
+          <div id="konfetti-github" class="d-flex justify-content-center align-items-center action-button">
+            Konfetti
+          </div>
         </div>
-
-        <div class="mt-2 row ms-0">
-    <div class="col bg-primary-subtle col-lg-2">col-sm</div>
-    <div class="col col-lg-2">col-sm</div>
-    <div class="col col-lg-2">col-sm</div>
-  </div>
 
       </div>
       <div class="modal-footer fw-lighter JetBrainsFooter">
--- index.php
+++ index.php
@@ -163,12 +163,12 @@
         <p class="mb-1 fw-bold fs-4">RonDev</p>
         <p class="mb-2 rotating-title">&lang;&frasl;<span id="fun-title">Pixel-Schubser</span>&rang;</p>
         <p class="mb-1 lh-sm fw-light">Zwischen Wahnsinn und Webseiten:<br>Programmieren ist für mich mehr als ein Hobby – es ist meine Art, mit einer überaktiven Birne und meiner PTBS umzugehen 🧠💻<br>Statt Therapie: HTML, PHP, CSS und gelegentliche Nervenzusammenbrüche. Arbeite immer mal wieder an Webprojekten – manche laufen rund, andere laufen Amok. Fortschritte werden gefeiert, Fails gnadenlos dokumentiert. Willkommen in meinem digitalen Selbsthilfeprogramm mit Stil und semikolongetriebener Selbsterkenntnis.</p>
-        <p class="mt-3 fw-light small text-lowercase text-break"><a href="https://rondev.de" target="_blank" class="link-light link-offset-2 link-underline-opacity-50 link-underline-opacity-100-hover">&lang;&frasl;Web&rang;</a> <a href="https://mastodon.social/@RonDev" class="link-light link-offset-2 link-underline-opacity-50 link-underline-opacity-100-hover" target="_blank">&lang;&frasl;Mastodon&rang;</a> <a href="#" class="link-light link-offset-2 link-underline-opacity-50 link-underline-opacity-100-hover" data-bs-toggle="modal" data-bs-target="#projektModal">&lang;&frasl;Weitere Projekte&rang;</a><br><a href="#" class="link-light link-offset-2 link-underline-opacity-50 link-underline-opacity-100-hover" data-bs-toggle="modal" data-bs-target="#kaffeeModal">&lang;&frasl;Kaffee spendieren&rang;</a></p>
+        <p class="mt-3 fw-light small text-lowercase text-break"><a href="https://rondev.de" rel="noreferrer noopener" target="_blank" class="link-light link-offset-2 link-underline-opacity-50 link-underline-opacity-100-hover">&lang;&frasl;Web&rang;</a> <a href="https://mastodon.social/@RonDev" rel="noreferrer noopener" class="link-light link-offset-2 link-underline-opacity-50 link-underline-opacity-100-hover" target="_blank">&lang;&frasl;Mastodon&rang;</a> <a href="#" class="link-light link-offset-2 link-underline-opacity-50 link-underline-opacity-100-hover" data-bs-toggle="modal" data-bs-target="#projektModal">&lang;&frasl;Weitere Projekte&rang;</a><br><a href="#" class="link-light link-offset-2 link-underline-opacity-50 link-underline-opacity-100-hover" data-bs-toggle="modal" data-bs-target="#kaffeeModal">&lang;&frasl;Kaffee spendieren&rang;</a></p>
       </div>
     </div>
 
     <div class="mb-3 text-center small JetBrains">
-    <?= copyright('1em', 'currentColor', 'svg-icon'); ?> <?php echo date("Y"); ?> Pixeljagd by <a href="https://rondev.de" class="custom-link" target="_blank">RonDev</a><br>
+    <?= copyright('1em', 'currentColor', 'svg-icon'); ?> <?php echo date("Y"); ?> Pixeljagd by <a href="https://rondev.de" rel="noreferrer noopener" class="custom-link" target="_blank">RonDev</a><br>
     </div>
 
   </div>
@@ -177,7 +177,7 @@
   </main>
 
   <footer class="text-center small JetBrains">
-    <?= copyright('1em', 'currentColor', 'svg-icon'); ?> <?php echo date("Y"); ?> Pixeljagd<sup><a href="" class="custom-link" data-bs-toggle="modal" data-bs-target="#versionModal">v.0.1</a></sup> by <a href="https://rondev.de" class="custom-link" target="_blank">RonDev</a><br>
+    <?= copyright('1em', 'currentColor', 'svg-icon'); ?> <?php echo date("Y"); ?> Pixeljagd<sup><a href="" class="custom-link" data-bs-toggle="modal" data-bs-target="#versionModal">v.0.1</a></sup> by <a href="https://rondev.de" rel="noreferrer noopener" class="custom-link" target="_blank">RonDev</a><br>
     Finde den kleinsten Pixel der Welt! <a class="" href="#" data-bs-toggle="modal" data-bs-target="#thanksModal">Danke</a>
   </footer>
 
